15bf7656a329517516c9076d63af149f595a5e02
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2017-07-15 15:25:49 +0200

    Reduce cache size

--- features/feature_sets.py
+++ features/feature_sets.py
@@ -120,7 +120,7 @@ class CachedAvgTokenCountFeatureSet(FeatureSet):
         
             yield vec
 
-    @lru_cache(maxsize=10000)
+    @lru_cache(maxsize=200)
     def _tokenize(self, text) -> List[str]:
         return list(self._chunk_tokenizer.tokenize(text))
 
