387847c03e2e75ce45871e07091dc27f3609012c
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2019-06-18 16:22:40 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="authorship-unmasking",
+    version="1.0.0",
+    author_email="janek.bevendorff@uni-weimar.de",
+    url="https://github.com/webis-de/unmasking",
+    description="General-purpose authorship unmasking framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
