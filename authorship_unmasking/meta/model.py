b03daa7ac3e52c5a3e84793d3b64749e96a25c37
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2020-03-02 16:02:44 +0100

    Make optimize() compatible with any iterable

--- authorship_unmasking/meta/model.py
+++ authorship_unmasking/meta/model.py
@@ -60,7 +60,7 @@ class LinearMetaClassificationModel(MetaClassificationModel):
         }
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            grid = GridSearchCV(estimator, parameters, cv=min(5, *np.bincount(y.astype(int))), n_jobs=-1)
+            grid = GridSearchCV(estimator, parameters, cv=min(5, *np.bincount(np.array(y, int))), n_jobs=-1)
             grid.fit(X, y)
         self._clf_params = grid.best_estimator_.get_params()
 
